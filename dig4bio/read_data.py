f3d87ba873415b6bf016d0a3e1c7b847c1093113
Jake Brannigan <71458482+J-Brannigan@users.noreply.github.com>
2026-05-31 13:39:52 +0100

    Initial commit

---
+++ dig4bio/read_data.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from pathlib import Path
+from dig4bio.paths import RAW_DATA_FOLDER
+
+
+def read_raman_file(file_name: str, level: str) -> pd.DataFrame:
+
+    folder_map = {
+        'raw': RAW_DATA_FOLDER
+    }
+
+    abbr_map = {
+        'anton532': 'anton_532',
+        'anton785': 'anton_785',
+        'mettler': 'mettler_toledo',
+        'tec' : 'tec5'
+    }
+
+    folder = folder_map[level]
+
+    candidates = [
+        folder / file_name,
+        folder / f"{file_name}.csv",
+    ]
+
+    if file_name in abbr_map:
+        candidates.append(folder / f"{abbr_map[file_name]}.csv")
+
+    for file_path in candidates:
+        if file_path.exists():
+            return pd.read_csv(file_path)
+    
+    raise FileNotFoundError()
