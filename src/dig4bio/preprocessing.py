222051941f1758a243c6cea48e1090d06963d987
Jake Brannigan <71458482+J-Brannigan@users.noreply.github.com>
2026-06-19 12:40:02 +0100

    Swapping out legacy scipy interpolate function v2

--- src/dig4bio/preprocessing.py
+++ src/dig4bio/preprocessing.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import numpy as np
-from scipy.interpolate import interp1d
+from scipy.interpolate import make_interp_spline
 from dig4bio.constants import FINGERPRINT_GRID
 
 def interpolate_spectra_to_grid(spectra: np.ndarray, old_wavenumbers: np.ndarray, new_wavenumbers: np.ndarray = FINGERPRINT_GRID) -> np.ndarray:
-    """Interpolate spectra from one wavenumber grid to another."""
-    f = interp1d(old_wavenumbers, spectra, axis=1, kind="linear", bounds_error=False, fill_value=np.nan)
-    return f(new_wavenumbers)
+    """Linearly interpolate spectra onto a common wavenumber grid."""
+
+    f = make_interp_spline(old_wavenumbers,spectra,k=1,axis=1,check_finite=True)
+
+    return f(new_wavenumbers,extrapolate=False)
